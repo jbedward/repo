8a79adb86eeeb008c5a31de7ee43f17fa7dc4454
root <root@chef-workstation>
2016-08-01 16:34:09 +0100

    Role config

--- roles/media_servers.rb
+++ roles/media_servers.rb
@@ -1,8 +1,3 @@
 name "media_servers"
 description "This role contains nodes, which act as media servers"
-run_list "recipe[ntp]"
-default_attributes 'ntp' => {
-'ntpdate' => {
-'disable' => true
-}
-}
+run_list "recipe[freeswitch]"
